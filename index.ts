fe5e01f508ad9a7954906792c710053d36884e96
Nijeesh Joshy <nijeesh4all@gmail.com>
2023-09-28 16:57:35 +0000

    added bucket and cloudfront routes

--- index.ts
+++ index.ts
@@ -1,38 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as apigateway from "@pulumi/aws-apigateway";
 import { lambda } from "@pulumi/aws";
 
-import { createUrlHandler, getUrlHandler } from "./api_gate_way_lambda_handler";
-import urls_table from "./urls_table";
+import urls_table from "./src/urls_table";
+import api from './src/api'
+import assets_bucket from './src/site/index';
 
-const helloHandler = new lambda.CallbackFunction("hello-handler", {
-  callback: async (ev, ctx) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, API Gateway!" })
-    };
-  },
-});
-
-// Define an endpoint that invokes a lambda to handle requests
-const api = new apigateway.RestAPI("api", {
-  routes: [
-    {
-      path: "test",
-      method: "GET",
-      eventHandler: helloHandler,
-    },
-    {
-      path: "url",
-      method: "POST",
-      eventHandler: createUrlHandler,
-    },
-    {
-      path: "url/{urlId}",
-      method: "GET",
-      eventHandler: getUrlHandler,
-    },
-  ],
-});
 
 export default api.url;
+exports.bucket_name = assets_bucket.bucket
+exports.bucket_website = assets_bucket.websiteEndpoint
