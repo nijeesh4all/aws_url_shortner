6b2fc2025bd5291a47c0660f589fa943cdf84303
Nijeesh Joshy <nijeesh4all@gmail.com>
2023-09-28 17:04:49 +0000

    added proper shortended url

--- src/api_gate_way_lambda_handlers.ts
+++ src/api_gate_way_lambda_handlers.ts
@@ -1,5 +1,6 @@
 import * as aws from "@pulumi/aws";
 import { retrive_url, store_url } from "./urls_table";
+import api from "./api";
 
 function if_valid_http_url(string: string): boolean {
   try {
@@ -10,56 +11,65 @@ function if_valid_http_url(string: string): boolean {
   }
 }
 
-function response(json_body:object, status_code: number){
-    return {
-        statusCode: status_code,
-        body: JSON.stringify(json_body)
-    }
+function response(json_body: object, status_code: number) {
+  return {
+    statusCode: status_code,
+    body: JSON.stringify(json_body),
+  };
 }
 
-function success_response(message: string, status_code: number, body:object) {
-    return response(
-        {
-            message,
-            error: false,
-            ...body
-        },
-        status_code
-    )
+function success_response(message: string, status_code: number, body: object) {
+  return response(
+    {
+      message,
+      error: false,
+      ...body,
+    },
+    status_code
+  );
 }
 
 function error_response(message: string, status_code: number) {
-    return response(
-        {
-            message,
-            error: true
-        },
-        status_code
-    )
+  return response(
+    {
+      message,
+      error: true,
+    },
+    status_code
+  );
 }
 
 const createUrlHandler = new aws.lambda.CallbackFunction("createUrlHandler", {
   memorySize: 256 /*MB*/,
   callback: async function (event: any, context) {
     console.log("event.body", event.body);
-    
-    const url = atob(event.body);
-    console.log("url", url);
 
-    if (!url) {
+    const original_url = atob(event.body);
+    console.log("url", original_url);
+
+    if (!original_url) {
       return error_response("empty url", 400);
     }
 
-    if (!if_valid_http_url(url)) {
+    if (!if_valid_http_url(original_url)) {
       return error_response("invalid url", 400);
     }
 
-    const response = await store_url(url)
-    if(!response){
-        return error_response("could not create", 400)
+    const response = await store_url(original_url);
+    if (!response) {
+      return error_response("could not create", 400);
     }
 
-    return success_response( "created",  201, response);
+    const { id, createdAt, url } = response.Item;
+
+    const application_url:string = api.url.get();
+
+    return success_response("created", 201, {
+      id,
+      createdAt,
+      url,
+      short_url: `${application_url}/${id}`,
+    });
   },
 });
 
